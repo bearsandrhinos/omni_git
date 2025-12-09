0a8c1b3bdd0c83a04f115b50230f5cbc166f741e
GitHub Action <action@github.com>
2025-12-09 18:47:37 +0000

    chore: auto-generate semantic views from Omni topics [skip ci]

--- terraform/omni_order_items_embed_sv.tf
+++ terraform/omni_order_items_embed_sv.tf
@@ -365,7 +365,7 @@ resource "snowflake_semantic_view" "omni_order_items_embed_sv" {
   metrics {
     semantic_expression {
       qualified_expression_name = "ECOMM_ORDER_ITEMS.total_sale_price"
-      sql_expression            = "SUM(ECOMM_ORDER_ITEMS.SALE_PRICE * 0.72)"
+      sql_expression            = "SUM(ECOMM_ORDER_ITEMS.SALE_PRICE * 0.99)"
     }
   }
 
